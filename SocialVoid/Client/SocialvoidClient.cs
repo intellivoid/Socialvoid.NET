a99696f932605f8ac4d13cd2b89087e7997ddbce
Sayan Biswas <sayan@pokurt.me>
2021-09-07 00:04:35 +0530

    Client: try to plan out a basic structure.

---
+++ SocialVoid/Client/SocialvoidClient.cs
@@ -0,0 +1,22 @@
+﻿using SocialVoid.Methods;
+
+namespace SocialVoid.Client
+{
+    class SocialvoidClient
+    {
+        public string PUB_HASH, PVT_HASH, SOCIALVOID_PLATFORM, SOCIALVOID_VERSION;
+        public SocialvoidClient(string public_hash, string private_hash, string platform, string version)
+        {
+            PUB_HASH = public_hash;
+            PVT_HASH = private_hash;
+            SOCIALVOID_PLATFORM = platform;
+            SOCIALVOID_VERSION = version;
+
+
+            bool createsession()
+            {
+                return Methods.CreateSession.Do(PUB_HASH, PVT_HASH, SOCIALVOID_PLATFORM, SOCIALVOID_VERSION);
+            }
+        }
+    }
+}
